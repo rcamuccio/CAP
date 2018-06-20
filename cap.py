7be3d9439d63de611ba3f46fb380def5acfbdc63
rcamuccio <rcamuccio@gmail.com>
2018-06-20 00:09:37 -0500

    Added logging system for output values

--- cap.py
+++ cap.py
@@ -4,7 +4,7 @@
 # Richard Camuccio
 # 9 Jun 2018
 #
-# Last update: 10 Jun 2018
+# Last update: 19 Jun 2018
 #
 # CAP
 # CCD analysis pipeline
@@ -32,7 +32,7 @@ def do_argparse():
 
 	return input_dir
 
-def do_combine(frame_list):
+def do_combine(frame_list, log):
 
 	print("<STATUS> Combining frames in list", frame_list, "...")
 	print()
@@ -40,27 +40,25 @@ def do_combine(frame_list):
 
 	return combined_frame
 
-def do_stat(array, name):
+def do_stat(array, name, log):
 
 	mean = np.mean(array)
-	print("Mean of", name, "=", round(mean,3 ))
+	log.write("Mean of " + str(name) + " = " + str(round(mean, 3)) + "\n")
 
 	median = np.median(array)
-	print("Median of", name, "=", round(median, 3))
+	log.write("Median of " + str(name) + " = " + str(round(median, 3)) + "\n")
 
 	stddev = np.std(array)
-	print("STD of", name, "=", round(stddev, 3))
+	log.write("STD of " + str(name) + " = " + str(round(stddev, 3)) + "\n")
 
 	minimum = np.min(array)
-	print("Min of ", name, "=", round(minimum, 3))
+	log.write("Min of " + str(name) + " = " + str(round(minimum, 3)) + "\n")
 
 	maximum = np.max(array)
-	print("Max of", name, "=", round(maximum, 3))
+	log.write("Max of " + str(name) + " = " + str(round(maximum, 3)) + "\n")
 
 	length = np.size(array)
-	print("NPIX of", name, "=", length)
-
-	print()
+	log.write("NPIX of " + str(name) + " = " + str(length) + "\n\n")
 
 	return mean, median, stddev, minimum, maximum, length
 
@@ -69,11 +67,12 @@ def main():
 	import time
 	start = time.time()
 
-	input_dir = do_argparse()	
+	input_dir = do_argparse()
+	log = open("log.txt", "w")	
 
 
 	#
-	# Bias analysis routine
+	# BIAS ANALYSIS
 	#
 	bias_dir = input_dir + "/bias"
 	bias_list = []
@@ -83,33 +82,50 @@ def main():
 	for frame in glob.glob("*.fit"):
 		bias_list.append(frame)
 
-	test_bias_1 = bias_list[0]
-	test_bias_1_fits = fits.open(str(bias_dir) + "/" + str(test_bias_1))
-	test_bias_1_array = test_bias_1_fits[0].data
-	test_bias_1_array = test_bias_1_array.astype(float)
+	bias_1 = bias_list[0]
+	bias_1_fits = fits.open(str(bias_dir) + "/" + str(bias_1))
+	bias_1_array = bias_1_fits[0].data
+	bias_1_array = bias_1_array.astype(float)
 
-	test_bias_2 = bias_list[1]
-	test_bias_2_fits = fits.open(str(bias_dir) + "/" + str(test_bias_2))
-	test_bias_2_array = test_bias_2_fits[0].data
-	test_bias_2_array = test_bias_2_array.astype(float)
+	bias_2 = bias_list[1]
+	bias_2_fits = fits.open(str(bias_dir) + "/" + str(bias_2))
+	bias_2_array = bias_2_fits[0].data
+	bias_2_array = bias_2_array.astype(float)
 
-	print("<STATUS> Subtracting frames " + str(test_bias_1) + " and " + str(test_bias_2) + " ...")
+	print("<STATUS> Subtracting frames " + str(bias_1) + " and " + str(bias_2) + " ...")
 	print()
-	diff_bias_array = np.subtract(test_bias_1_array, test_bias_2_array)
+	diff_bias_array = np.subtract(bias_1_array, bias_2_array)
 
-	diff_stddev = do_stat(diff_bias_array, "difference bias array")[2]
+	diff_stddev = do_stat(diff_bias_array, "difference bias array", log)[2]
 	readout_noise = diff_stddev / math.sqrt(2)
-	print("<OUTPUT> Calculated readout noise: " + str(round(readout_noise, 3)) + " ADU")
-	print()
+	log.write("Calculated readout noise from biases = " + str(round(readout_noise, 3)) + " ADU\n\n")
 
-	master_bias = do_combine(bias_list)
+	master_bias = do_combine(bias_list, log)
 	#print("<STATUS> Writing master bias to disk ...")
 	#print()
 	#ccdproc.fits_ccddata_writer(master_bias, str(bias_dir) + "/master-bias.fit", overwrite=True)
 
+	#
+	# HISTOGRAM OF BIAS
+	#
+	master_bias_data = master_bias[0].data
+	f_bias_array = master_bias_data.flatten()
+
+	bias_xrange = (994, 1083)
+	nbins = np.arange(min(f_bias_array), max(f_bias_array) + 1, 1)
+
+	font = {"fontname":"Monospace",
+		"size":10}
+
+	plt.hist(f_bias_array, range=bias_xrange, bins=nbins, histtype="step")
+	plt.title("Histogram of bias-avg.fit\nnbins=" + str(len(nbins)), **font)
+	plt.xticks(**font)
+	plt.yticks(**font)
+	plt.savefig(str(bias_dir) + "/bias-hist.png")
+
 
 	#
-	# Dark analysis routine
+	# DARK ANALYSIS
 	#
 	dark_dir = input_dir + "/dark"
 	dark_list = []
@@ -135,9 +151,9 @@ def main():
 	print()
 	diff_dark_array = np.subtract(dark_min_bias_1_array, dark_min_bias_2_array)
 
-	do_stat(dark_min_bias_1_array, "dark 1 minus master bias")
-	do_stat(dark_min_bias_2_array, "dark 2 minus master bias")
-	do_stat(diff_dark_array, "difference dark array")
+	do_stat(dark_min_bias_1_array, "dark 1 minus master bias", log)
+	do_stat(dark_min_bias_2_array, "dark 2 minus master bias", log)
+	do_stat(diff_dark_array, "difference dark array", log)
 
 	#print("<STATUS> Writing differenced dark frame 1 to disk ...")
 	#print()
@@ -181,7 +197,7 @@ def main():
 	#
 	flat_half_fits = fits.open(flat_half_list[0])
 	flat_half_exptime = flat_half_fits[0].header['EXPTIME']
-	flat_half_combined = do_combine(flat_half_list)
+	flat_half_combined = do_combine(flat_half_list, log)
 
 	#print("<STATUS> Writing combined half-saturation flat to disk ...")
 	#print()
@@ -195,7 +211,7 @@ def main():
 	flat_avg_value = np.mean(flat_min_dark_array)
 	flatfield_array = flat_min_dark_array / flat_avg_value
 
-	do_stat(flatfield_array, "flatfield")
+	do_stat(flatfield_array, "flatfield", log)
 
 	#print("<STATUS> Writing flatfield to disk ...")
 	#print()
@@ -205,7 +221,6 @@ def main():
 	#
 	# HISTOGRAM OF FLATFIELD
 	#
-
 	flat_ff_array = flatfield_array.flatten()
 
 	flatfield_xrange = (0.95, 1.05)
@@ -218,41 +233,18 @@ def main():
 	plt.title("Histogram of flatfield.fit\nnbins=" + str(nbins), **font)
 	plt.xticks(**font)
 	plt.yticks(**font)
-	plt.show()
+	plt.savefig(str(flat_dir) + "/flat_half/flat-hist.png")
 
+
+
+	for x in os.walk(flat_linear_dir):
+		print(x[0])
+		print()
+
+	log.close()
 
 	end = time.time()
 	print(str(end - start) + " seconds to complete.")
 
+
 main()
-
-"""
-bias = fits.open("20180606/bias/bias-avg.fit")
-flatfield = fits.open("20180606/flat/flatfield.fit")
-
-bias_array = bias[0].data
-flatfield_array = flatfield[0].data
-
-f_bias_array = bias_array.flatten()
-f_flat_array = flatfield_array.flatten()
-
-bias_xrange = (994, 1083)
-flatfield_xrange = (0.95, 1.05)
-nbins = 500
-nbins_bias = np.arange(min(f_bias_array), max(f_bias_array) + 1, 1)
-
-font = {"fontname":"Monospace",
-		"size":10}
-
-plt.hist(f_bias_array, range=bias_xrange, bins=nbins_bias, histtype="step")
-plt.title("Histogram of bias-avg.fit\nnbins=" + str(len(nbins_bias)), **font)
-plt.xticks(**font)
-plt.yticks(**font)
-plt.show()
-
-plt.hist(f_flat_array, range=flatfield_xrange, bins=nbins, histtype="step")
-plt.title("Histogram of flatfield.fit\nnbins=" + str(nbins), **font)
-plt.xticks(**font)
-plt.yticks(**font)
-plt.show()
-"""
